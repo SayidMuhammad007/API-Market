8f350d76a74e4bf1def0fb9ec31b24cc59243967
SayidMuhammad007 <uzbekcoder1507@gmail.com>
2024-03-19 10:13:06 +0500

    asd

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -237,7 +237,7 @@ class BasketController extends Controller
         }
 
         $product = Store::find($request->product_id);
-        if ($basket['quantity'] > $product['quantity']) {
+        if ($request['quantity'] > $product['quantity']) {
             return response()->json(['error' => 'Insufficient stock. Available quantity: ' . $product->quantity], 400);
         }
         // decrement product quantity
