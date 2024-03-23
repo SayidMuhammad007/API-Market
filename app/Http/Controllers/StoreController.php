5b6afb5c498c7c92f2a4b2e61142a793df3e7986
SayidMuhammad007 <uzbekcoder1507@gmail.com>
2024-03-23 11:58:17 +0500

    asd

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -36,7 +36,7 @@ class StoreController extends Controller
         }
 
         // Paginate the results
-        $stores = $query->orderBy("id",'DESC')->paginate(10);
+        $stores = $query->orderBy("id", 'DESC')->paginate(10);
 
         return response()->json($stores);
     }
@@ -90,12 +90,15 @@ class StoreController extends Controller
             ], 400);
         }
         $item = Store::create(array_merge($request->all(), ['barcode' => $branch->barcode, 'branch_id' => $branch->id]));
+        $item->update([
+            'branch_id' => $branch->id,
+        ]);
         if ($request->hasFile('image') && $request->file('image')->isValid()) {
             $item->addMediaFromRequest('image')->toMediaCollection('images');
         }
         $branch->update(['barcode' => ++$branch->barcode]);
 
-        return response()->json(Store::with(['media', 'category', 'branch', 'price'])->where('branch_id', auth()->user()->branch_id)->where('status', 1)->orderBy("id","DESC")->paginate(20), 201);
+        return response()->json(Store::with(['media', 'category', 'branch', 'price'])->where('branch_id', auth()->user()->branch_id)->where('status', 1)->orderBy("id", "DESC")->paginate(20), 201);
     }
 
     /**
@@ -168,7 +171,7 @@ class StoreController extends Controller
             $item->addMediaFromRequest('image')->toMediaCollection('images');
         }
 
-        return response()->json(Store::with(['media', 'category', 'branch', 'price'])->where('status', 1)->where('branch_id', auth()->user()->branch_id)->orderBy("id",'DESC')->paginate(20));
+        return response()->json(Store::with(['media', 'category', 'branch', 'price'])->where('status', 1)->where('branch_id', auth()->user()->branch_id)->orderBy("id", 'DESC')->paginate(20));
     }
 
     /**
@@ -181,7 +184,7 @@ class StoreController extends Controller
                 'status' => '0'
             ]);
         }
-        return response()->json(Store::with(['media', 'category', 'branch', 'price'])->where('branch_id', auth()->user()->branch_id)->where('status', 1)->orderBy("id","DESC")->paginate(20));
+        return response()->json(Store::with(['media', 'category', 'branch', 'price'])->where('branch_id', auth()->user()->branch_id)->where('status', 1)->orderBy("id", "DESC")->paginate(20));
     }
 
     public function calculate()
