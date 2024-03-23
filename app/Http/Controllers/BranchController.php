6a2797e58811d1e721327055c38a460dc0519126
SayidMuhammad007 <uzbekcoder1507@gmail.com>
2024-03-23 10:21:35 +0500

    test

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -127,7 +127,7 @@ class BranchController extends Controller
             }
             // }
 
-            return response()->json(Store::with(['media', 'category', 'branch', 'price'])->where('id', auth()->user()->branch_id)->paginate(20), 201);
+            return response()->json(Store::with(['media', 'category', 'branch', 'price'])->where('branch_id', auth()->user()->branch_id)->paginate(20), 201);
         }
     }
 }
