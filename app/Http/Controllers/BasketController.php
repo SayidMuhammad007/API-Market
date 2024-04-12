772f9860a34349d04d0d33b3a9a5f59667f4863a
SayidMuhammad007 <uzbekcoder1507@gmail.com>
2024-04-12 18:54:38 +0500

    test

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -141,14 +141,14 @@ class BasketController extends Controller
             $basket = $user->baskets()->where('status', 0)->first();
 
             // Check price and update basket and order accordingly
-            if ($item['price_id'] == 1 && (float)$item['price'] > (float)$inUzs && !$request->price) {
+            if ($item['price_id'] == 1 && (float)$item['price'] > (float)$inUzs && !$request->price && $item['type_id'] !== 5) {
                 return response()->json([
                     'success' => false,
                     'message' => 'So`mda qiymat oshib ketdi'
                 ], 400);
             }
 
-            if ($item['price_id'] == 2 && $item['price'] > $inDollar && !$request->price) {
+            if ($item['price_id'] == 2 && $item['price'] > $inDollar && !$request->price && $item['type_id'] !== 5) {
                 return response()->json([
                     'success' => false,
                     'message' => 'Dollarda qiymat oshib ketdi'
