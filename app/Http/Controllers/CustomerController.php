e44ae741e5d247b671562652f656761340dff8d5
SayidMuhammad007 <uzbekcoder1507@gmail.com>
2024-04-21 08:46:14 +0500

    test

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -92,12 +92,12 @@ class CustomerController extends Controller
         $payments_dollar = 0;
         foreach ($customer->customerLog() as $val) {
             if($val->type_id == 4 && $val->price_id == 1){
-                $dollar = Price::where('start', '>=', $val->created_at)->where('finish', '<=', $val->created_at)->value('price')->first();
+                $dollar = Price::where('start', '<=', $val->created_at)->where('finish', '>', $val->created_at)->value('price')->first();
                 $debts_dollar = $debts_dollar + $val->price * $dollar;
             }else if($val->type_id == 4 && $val->price_id == 2){
                 $debts_dollar = $debts_dollar + $val->price;
             }else if($val->type_id != 4 && $val->price_id == 1){
-                $dollar = Price::where('start', '>=', $val->created_at)->where('finish', '<=', $val->created_at)->value('price')->first();
+                $dollar = Price::where('start', '<=', $val->created_at)->where('finish', '>', $val->created_at)->value('price')->first();
                 $payments_dollar = $payments_dollar + $val->price * $dollar;
             }else if($val->type_id != 4 && $val->price_id == 2){
                 $payments_dollar = $payments_dollar + $val->price;
