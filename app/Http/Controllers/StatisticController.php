25f90cbc744a5688016ba21bf1fa3dc5d72a210d
SayidMuhammad007 <uzbekcoder1507@gmail.com>
2024-04-21 16:22:12 +0500

    calc

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -214,20 +214,30 @@ class StatisticController extends Controller
                   INNER JOIN avtozapchast_new.orders ON avtozapchast_new.baskets.order_id = avtozapchast_new.orders.id
                   WHERE avtozapchast_new.orders.branch_id = avtozapchast_new.branches.id AND DATE(avtozapchast_new.basket_prices.created_at) BETWEEN ? AND ?  AND price_id = 1)
                 ) as benefit_uzs,
+
                 ((SELECT SUM(price) FROM avtozapchast_new.order_prices 
-                INNER JOIN avtozapchast_new.orders ON avtozapchast_new.order_prices.order_id = avtozapchast_new.orders.id
-                WHERE avtozapchast_new.orders.branch_id = avtozapchast_new.branches.id AND DATE(avtozapchast_new.order_prices.created_at) BETWEEN ? AND ?  AND price_id = 2) -  
-               (SELECT SUM(price_come) FROM avtozapchast_new.basket_prices 
-                INNER JOIN avtozapchast_new.baskets ON avtozapchast_new.basket_prices.basket_id = avtozapchast_new.baskets.id
-                INNER JOIN avtozapchast_new.orders ON avtozapchast_new.baskets.order_id = avtozapchast_new.orders.id
-                WHERE avtozapchast_new.orders.branch_id = avtozapchast_new.branches.id AND DATE(avtozapchast_new.basket_prices.created_at) BETWEEN ? AND ?  AND price_id = 2)
-              ) as benefit_usd,
+                  INNER JOIN avtozapchast_new.orders ON avtozapchast_new.order_prices.order_id = avtozapchast_new.orders.id
+                  WHERE avtozapchast_new.orders.branch_id = avtozapchast_new.branches.id AND DATE(avtozapchast_new.order_prices.created_at) BETWEEN ? AND ?  AND price_id = 2) -  
+                 (SELECT SUM(price_come) FROM avtozapchast_new.basket_prices 
+                  INNER JOIN avtozapchast_new.baskets ON avtozapchast_new.basket_prices.basket_id = avtozapchast_new.baskets.id
+                  INNER JOIN avtozapchast_new.orders ON avtozapchast_new.baskets.order_id = avtozapchast_new.orders.id
+                  WHERE avtozapchast_new.orders.branch_id = avtozapchast_new.branches.id AND DATE(avtozapchast_new.basket_prices.created_at) BETWEEN ? AND ?  AND price_id = 2)
+                ) as benefit_usd,
+
                 (SELECT SUM(cost) FROM avtozapchast_new.expences 
-                 WHERE avtozapchast_new.expences.branch_id = avtozapchast_new.branches.id AND DATE(avtozapchast_new.expences.created_at) BETWEEN ? AND ?) as expence')
-                ->setBindings([$start, $finish, $start, $finish, $start, $finish, $start, $finish, $start, $finish, $start, $finish, $start, $finish, $start, $finish])
+                 WHERE avtozapchast_new.expences.branch_id = avtozapchast_new.branches.id AND DATE(avtozapchast_new.expences.created_at) BETWEEN ? AND ?   AND price_id = 1) as expence_uzs,
+                 
+                 (SELECT SUM(cost) FROM avtozapchast_new.expences 
+                 WHERE avtozapchast_new.expences.branch_id = avtozapchast_new.branches.id AND DATE(avtozapchast_new.expences.created_at) BETWEEN ? AND ?   AND price_id = 2) as expence_usd
+                 ')
+                ->setBindings([$start, $finish, $start, $finish, $start, $finish, $start, $finish, $start, $finish, $start, $finish, $start, $finish, $start, $finish, $start, $finish, $start, $finish])
                 ->get();
 
-            return response()->json($branches);
+            return response()->json([
+                'start' => $start,
+                'finish' => $finish,
+                'data' => $branches
+            ]);
         } else {
             return response()->json([
                 'success' => false,
