468b6d59c71f8670f8ae4254d546fcb113de24ff
SayidMuhammad007 <uzbekcoder1507@gmail.com>
2024-04-25 12:46:24 +0500

    test

--- routes/api.php
+++ routes/api.php
@@ -78,8 +78,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/company/debt/{company}/delete', [CompanyController::class, 'deleteDebt']);
     Route::post('/company/{company}/stores/attach', [CompanyController::class, 'storeToCompany']);
     Route::post('/company/{company}/stores/add', [CompanyController::class, 'addStore']);
-    Route::post('/company/{company}/stores', [CompanyController::class, 'stores']);
-    Route::get('/company', [CompanyController::class, 'store']);
+    Route::get('/company/{company}/stores', [CompanyController::class, 'stores']);
+    Route::post('/company', [CompanyController::class, 'store']);
     Route::delete('/company/{company}', [CompanyController::class, 'destroy']);
 
     // Type routes
