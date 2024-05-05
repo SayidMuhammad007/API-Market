230f50e136f1af51997ae6210a6e5e2f74794ad0
SayidMuhammad007 <uzbekcoder1507@gmail.com>
2024-05-05 09:32:00 +0500

    test

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -195,11 +195,18 @@ class StatisticController extends Controller
                  WHERE orders.branch_id = branches.id 
                  AND DATE(order_prices.created_at) BETWEEN ? AND ? AND price_id = 2) as sell_price_usd,
 
-                (SELECT WHEN CASE (SELECT price_id FROM stores WHERE id = store_id) == 1 THEN SUM(price_come) ELSE 0 END FROM basket_prices 
-                 INNER JOIN baskets ON basket_prices.basket_id = baskets.id
-                 INNER JOIN orders ON baskets.order_id = orders.id
-                 WHERE orders.branch_id = branches.id 
-                 AND DATE(basket_prices.created_at) BETWEEN ? AND ? AND price_id = 1) as come_price_uzs,
+                 SELECT COALESCE(
+                    (SELECT CASE 
+                        WHEN (SELECT price_id FROM stores WHERE id = store_id) = 1 
+                        THEN SUM(price_come) 
+                        ELSE 0 
+                     END
+                     FROM basket_prices 
+                     INNER JOIN baskets ON basket_prices.basket_id = baskets.id
+                     INNER JOIN orders ON baskets.order_id = orders.id
+                     WHERE orders.branch_id = branches.id 
+                     AND DATE(basket_prices.created_at) BETWEEN ? AND ? 
+                     AND price_id = 1), 0) as come_price_uzs,                
 
                  (SELECT SUM(price_come) FROM basket_prices 
                  INNER JOIN baskets ON basket_prices.basket_id = baskets.id
